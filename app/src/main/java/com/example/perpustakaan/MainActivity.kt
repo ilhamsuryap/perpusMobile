58fbd976b50fb47ae2a7d59237daf6f085f8696f
alzamna <alzamna195@gmail.com>
2024-10-17 07:55:27 +0700

    login

--- app/src/main/java/com/example/perpustakaan/MainActivity.kt
+++ app/src/main/java/com/example/perpustakaan/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.perpustakaan
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonLogin = findViewById<Button>(R.id.btnLogin)
+        val editUsername = findViewById<EditText>(R.id.inputusr)
+        val editPassword = findViewById<EditText>(R.id.inputpss)
+
+        val correctUsername = "admin"
+        val correctPassword = "123"
+
+        buttonLogin.setOnClickListener {
+            val username = editUsername.text.toString()
+            val password = editPassword.text.toString()
+
+            if (username == correctUsername && password == correctPassword) {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
