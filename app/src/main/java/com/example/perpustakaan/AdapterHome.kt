d854dd819a0ac25a79a6a1755baf9eacdf18b285
alzamna <alzamna195@gmail.com>
2024-11-10 21:10:02 +0700

    menambahkan recycler view type baru (iklan)

--- app/src/main/java/com/example/perpustakaan/AdapterHome.kt
+++ app/src/main/java/com/example/perpustakaan/AdapterHome.kt
@@ -12,33 +12,55 @@ import com.example.perpustakaan.Dao.Buku
 import com.example.perpustakaan.R
 import com.example.perpustakaan.database.PerpustakaanDatabase
 
-class AdapterHome(private val context: Context, private val bookList: List<Buku>, private val database: PerpustakaanDatabase, private val onItemClickListener: (Buku) -> Unit) :
-    RecyclerView.Adapter<AdapterHome.HomeViewHolder>() {
+class AdapterHome(
+    private val context: Context,
+    private val bookList: List<Buku>,
+    private val database: PerpustakaanDatabase,
+    private val onItemClickListener: (Buku) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.cardhome, parent, false)
-        return HomeViewHolder(view)
+    private val VIEW_TYPE_BOOK = 0
+    private val VIEW_TYPE_AD = 1
+
+    override fun getItemViewType(position: Int): Int {
+        return if ((position + 1) % 6 == 0) VIEW_TYPE_AD else VIEW_TYPE_BOOK
     }
 
-    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        val book = bookList[position]
-        holder.bookTitle.text = book.judul
-//        holder.borrowerName.text = book.penulis
-        holder.bookImage.setImageResource(R.drawable.menara5negara)
-        holder.butoon.setOnClickListener{
-            onItemClickListener(book)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_AD) {
+            val view = LayoutInflater.from(context).inflate(R.layout.cardhome_ads, parent, false)
+            AdViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(context).inflate(R.layout.cardhome, parent, false)
+            HomeViewHolder(view)
         }
+    }
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == VIEW_TYPE_AD) {
+
+        } else {
+            val bookPosition = position - (position / 6)
+            val book = bookList[bookPosition]
+            (holder as HomeViewHolder).bookTitle.text = book.judul
+            holder.bookImage.setImageResource(R.drawable.menara5negara)
+            holder.butoon.setOnClickListener {
+                onItemClickListener(book)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        return bookList.size
+        return bookList.size + (bookList.size / 5)
     }
 
     inner class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val bookTitle: TextView = itemView.findViewById(R.id.text1)
-//        val borrowerName: TextView = itemView.findViewById(R.id.text2)
         val bookImage: ImageView = itemView.findViewById(R.id.imageView)
-        val butoon : Button = itemView.findViewById(R.id.button)
+        val butoon: Button = itemView.findViewById(R.id.button)
+    }
+
+    inner class AdViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     }
 }
+
